377df33c005bd9219da62e8a4d378683935a42a1
AngeloAkuhwa <angeloakuhwa@gmail.com>
2021-08-20 10:49:52 +0100

    created a program that prints my name

---
+++ PrintMyName/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrintMyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrintName("Angelo Akuhwa"));
+        }
+        public static string PrintName(string name)
+        {
+            return name;
+        }
+    }
+}
